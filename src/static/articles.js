435b50eb3654f4724c2fbd052db75620453cda21
Zach VanSomeren <zvansomeren@gmail.com>
2018-12-05 14:32:44 -0800

    Final article list and images

--- src/static/articles.js
+++ src/static/articles.js
@@ -8,9 +8,17 @@ import creditKarma from '../static/images/carousel/financial/credit-karma.jpg';
 
 // Import science section images
 import untamed from '../static/images/carousel/science/untamed.jpg';
+import altitude from '../static/images/carousel/science/altitude.jpg';
+import wildfire from '../static/images/carousel/science/wildfire.jpeg';
+import genetics from '../static/images/carousel/science/genetics.jpeg';
+import scientific from '../static/images/carousel/science/scientific.png';
 
 // Import travel section images
 import farAndWide from '../static/images/carousel/travel/farandwide.jpg';
+import hostel from '../static/images/carousel/travel/hostel.jpg';
+import maccu from '../static/images/carousel/travel/machu_picchu_peru.jpg';
+import traps from '../static/images/carousel/travel/traps.jpg';
+import housesitting from '../static/images/carousel/travel/housesitting.jpg';
 
 const financial = [
   {
@@ -57,6 +65,30 @@ const science = [
     url: 'http://www.untamedscience.com/biodiversity/zika-virus/',
     quote: `Deep in an African jungle, a monkey sits high in the canopy of a lush forest. This is a special monkey; he is called Rhesus 766. Every so often, people come to visit him. They give him food and water, clean his aerial pen, and check that he is healthy. One day, Rhesus 766 comes down with a fever. His visitors, concerned, bring him back to a building near the forest to run some tests. What they find is striking; a brand new, unknown virus is causing Rhesus 766’s symptoms.`,
     img: untamed,
+  },
+  {
+    title: 'How the Body Adjusts to Altitude',
+    url: 'http://www.untamedscience.com/biology/human/respiratory-system/altitude-acclimatization/',
+    quote: `When Felix Baumgartner shattered the world record for the highest skydiving jump at 127,852 feet in 2012, it was only a matter of time before it was broken again. Two years later, Alan Eustace, a top Google executive, went even further in an equally amazing (but less well-publicized) stunt: he attached himself to a helium balloon and jumped from 135,892 feet.`,
+    img: altitude,
+  },
+  {
+    title: 'How the West Was Burned: A Tale of Wildfires and Ecosystems in the American West',
+    url: 'http://www.untamedscience.com/biology/ecology/ecology-articles/western-wildfires/',
+    quote: `On May 1, 2016, a tiny spark was created near Fort McMurray, Alberta, Canada. It was like a lot of other sparks, but this one grew—and fast. Eventually it turned into a raging wildfire that forced tens of thousands of people to evacuate. It destroyed 2,400 homes and occupied parts of two Canadian provinces. What’s more—it’s still burning, more than a year later.`,
+    img: wildfire,
+  },
+  {
+    title: 'How do Mendelian genetics work?',
+    url: 'http://www.untamedscience.com/biology/genetics/mendelian-genetics/?fbclid=IwAR0EfE9D_ILplIXt7aWTzNu63s2ACjanUiykhiXGE2civuf6ElWHssG8Usc',
+    quote: `Think about the last time you went to the produce section in your grocery store. It’s chock full of all kinds of plants, and even bacteria and fungi. Now, think about the last time you saw a dog show on TV: each dog breed was bred for a purpose, just like each of the foods in the grocery store. All of these organisms required careful breeding to get them to where they are today, and we’re still constantly changing and tinkering with domesticated organisms. In order to breed organisms for their desired traits, we need to know how to reliably predict what’ll happen when you breed two things.`,
+    img: genetics,
+  },
+  {
+    title: 'What is the scientific method?',
+    url: 'http://www.untamedscience.com/biology/scientific-method/?fbclid=IwAR1mBtGsbVTG3RMgwYJQzm8aQcVTGcKWA3nkHjkvkIWWYh6FZ_Zl3iFK_sw',
+    quote: `What do you think about when you hear the words, “the scientific method?” Do you picture a bunch of dusty old men in lab coats, fiddling with beakers? Those scientists might be using the scientific method, but so are lots of other scientists doing all kinds of interesting, lively things. In fact, you yourself even use parts of the scientific method every day to make decisions. At its heart, the scientific method is just a process that scientists use to verify new facts. It’s sort of like a checklist, and by going through it one step at a time, you can be sure that you’re coming up with the right facts. No one wants to discover the cure for cancer only to find that they skipped a step and the “cure” actually doesn’t do anything.`,
+    img: scientific,
   }
 ];
 
@@ -67,6 +99,30 @@ const travel = [
     quote: `Ah, national parks. These majestic treasures have been beloved by millions since Yellowstone, our first national park, was established in 1872. Since then, 57 other glorious parks have been preserved and opened to the public, welcoming visitors to enjoy towering peaks, jagged red-rock formations, active volcanoes and so much more.`,
     img: farAndWide, 
   },
+  {
+    title: 'How to Stay in a Hostel',
+    url: 'https://www.societyofgrownups.com/blog/how-to-stay-in-a-hostel?fbclid=IwAR0IzUanfLOV4L5TAPUPePTWo8X9k-jPLK0oR0HXY_GxPZXHK5WU_5zH_Yk',
+    quote: `One of my biggest regrets about my twenties is that I didn’t have very many interesting travel experiences. When I turned 30 last year, I decided that had to change. I signed on for a three-week DIY trip to Peru with one of my good friends from grad school. In true Millennial fashion, we decided to do it as cheaply as possible and stay in backpacker-style hostels, a-la The Beach (but without all the shady business…OK, it wasn’t like The Beach at all). My only meaningful experience with hostels was watching the movie Hostel as a teenager (probably not a good idea).`,
+    img: hostel,
+  },
+  {
+    title: 'How to Get to Machu Picchu and Back for $30',
+    url: 'https://www.wisebread.com/how-to-get-to-machu-picchu-and-back-for-30?fbclid=IwAR15X-9NRrzJcMgn7VvzGuRSExf0e6XvaPUj7mMt5Mnzq2S7x_XeTIAl2fM',
+    quote: `Machu Picchu is a destination on almost everyone's bucket list, and rightly so. It's one of the seven wonders of the world. But even just getting to Machu Picchu is a hassle. There are no roads going there, meaning your travel options are limited. Most people get to Machu Picchu by train, but that'll cost you $130 or more round-trip. On top of that, you'll need to pay for entry into Machu Picchu itself, which runs about $40.`,
+    img: maccu,
+  },
+  {
+    title: '6 Travel ‘Savings’ Traps That Can End Up Costing You More',
+    url: 'https://grow.acorns.com/6-travel-savings-traps-that-can-end-up-costing-you-more/?fbclid=IwAR1Iy3-SCwcxgc0X_2l8q41HaNTJFRuDHkoZeGjSUKQJhWdibBrj-jpJ7Eo&gsi=H8GwMmg',
+    quote: `It’s easier than ever to pay less for amazing travel experiences. But that doesn’t mean you can always take what sounds like a great savings opportunity at face value. If you’re not careful, some “savings” tricks—like these six—can end up costing you more.`,
+    img: traps,
+  },
+  {
+    title: 'How House-Sitting Could Make Your Travel Dreams Come True',
+    url: 'https://learnvest.com/article/why-house-sitting-could-be-the-way-to-make-your-travel-dreams-come-true?fbclid=IwAR215Wm4C79U6_eLezhylc1ykUxQJs8ERf1tBHa8O2US98YRt9zTHqLAFD8',
+    quote: `Last year, Amy and Tim Rutherford took four trips across Colorado and California, visiting three different cities via car and plane for a total of 26 nights. The final price tag for these trips? $1,434.39. That’s $55.17 per day, Snickers bars included. Their secret: They’re house sitters. They stay in other people’s homes for free in exchange for caring for pets and property while homeowners are away. And though they started out locally, the couple is taking their business abroad in 2018 and traveling the world on a budget. Here’s how you could get this life.`,
+    img: housesitting,
+  }
 ];
 
 export { financial, science, travel };
